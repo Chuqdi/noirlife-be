ba708f0566fdcfe044bf2d7678b582ca572d95d3
chuqdi <morganhezekiah111@gmail.com>
2026-09-02 13:57:27 +0100

    last commit 1

--- users/models.py
+++ users/models.py
@@ -99,6 +99,7 @@ class ReferalCode(models.Model):
 
 class UserEmailActivationCode(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     code = models.CharField(max_length=6, null=False, blank=False)
     date_created = models.DateTimeField(default = timezone.now)
 
